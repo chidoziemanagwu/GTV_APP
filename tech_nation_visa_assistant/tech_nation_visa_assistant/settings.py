04b9d85b92ed52f35606ad00ab34f77f122ea46d
Mr. Managwu <39161025+chidoziemanagwu@users.noreply.github.com>
2025-05-21 08:16:36 +0100

    .

--- tech_nation_visa_assistant/tech_nation_visa_assistant/settings.py
+++ tech_nation_visa_assistant/tech_nation_visa_assistant/settings.py
@@ -21,6 +21,12 @@ if not ALLOWED_HOSTS or ALLOWED_HOSTS == ['']:
     ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'gtv-app.onrender.com', '*.onrender.com']
 
 
+# Add this to your settings.py
+CSRF_TRUSTED_ORIGINS = [
+    'https://gtv-app.onrender.com',
+    'https://*.onrender.com'
+]
+
 # Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
